5a4348eba2a4aa3f5a8c62063ed1f20d05646055
Divya Sharma <divya-sh@Divyas-MacBook-Air.local>
2022-10-19 18:17:14 -0400

    Dataset done; might want to switch to reading csv as neighbor calcs are slow

--- crystal_data.py
+++ crystal_data.py
@@ -1,97 +1,96 @@
 from pymatgen.analysis.local_env import CrystalNN
 from pymatgen.ext.matproj import MPRester
+from torch.utils.data import Dataset
 from pymatgen.core import Structure
 import os.path as osp
 import pandas as pd
 import warnings
+import torch
 import os
 
 API_KEY = 'ZOyRKuvwhXfX8jJPK'
-data_base = '/Users/divya-sh/Documents/' \
-             'MILA - AI for Materials/data/'
 mpr = MPRester(API_KEY)
 
+class CrystalDataset(Dataset):
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.cifs = os.listdir(data_path)
+
+    def oracle_data(self, crystal):
+        nn_calc = CrystalNN()
+        li_count = 0
+        llb = 0
+        sbi = 0
+        bond_count = 0
+        lasd = 0
+        llsd = 0
+        afc = 0
+        anion_count = 0
+        # raise Exception
+        for s, site in enumerate(crystal.get_all_neighbors(8)):
+            if crystal[s].specie.Z == 3:
+                llsd += min([neigh[1] for neigh in site if crystal[s].specie.Z == 3])
+                lasd += min([neigh[1] for neigh in site if neigh.specie.oxi_state <= 0])
+
+        for e, elem in enumerate(crystal):
+            nb = crystal.get_neighbors(elem, 4)
+            if elem.specie.Z == 3:
+                li_count += 1
+                llb += sum([i.specie.Z  == 3 for i in nb])
+            else:
+                bond_count += len(nb)
+                sbi += sum([abs(elem.specie.X - i.specie.X) for i in nb])
+            if elem.specie.oxi_state <= 0:
+                anion_count += 1
+                afc += nn_calc.get_cn(crystal, e)
+
+        llb = llb / li_count
+        lasd = lasd / li_count
+        llsd = llsd / li_count
+        sbi = sbi / bond_count
+        afc = afc / anion_count
+
+        return (llb, sbi, afc, lasd, llsd)
+        # return (lasd, llsd)
+
+    def building_blocks(self, crystal):
+        # print(crystal.formula)
+        composition = {i:0 for i in range(1, 119)}
+        denom = len(crystal)
+        
+        for elem in crystal:
+            e = elem.specie.Z
+            # print(type(e))
+        composition[e] += 1 / denom
+
+        # print(crystal.get_space_group_info()[0])
+        composition['space_group'] = crystal.get_space_group_info()[1]
+        return composition
+
+    def __getitem__(self, idx):
+        warnings.filterwarnings('ignore')
+        struc = osp.join(self.data_path, self.cifs[idx])
+        struc = Structure.from_file(struc)
+        struc.add_oxidation_state_by_guess()
+        oracle = self.oracle_data(struc)
+        bb = self.building_blocks(struc)
+
+        return self.cifs[idx], torch.tensor(oracle) , torch.tensor(list(bb.values()))
+
+
 def download(criteria, properties, save_dir):
-    write_path = osp.join(data_base, save_dir)
+    write_path = osp.join(save_dir)
     for i, d in enumerate(mpr.query(criteria=criteria, properties=["material_id" , "cif"] + properties)):
         with open(osp.join(write_path, d['material_id'] + '.cif'), 'w+') as fobj:
             fobj.write(d['cif'])
 
-def oracle_data(crystal):
-    nn_calc = CrystalNN()
-    li_count = 0
-    llb = 0
-    sbi = 0
-    bond_count = 0
-    lasd = 0
-    llsd = 0
-    afc = 0
-    anion_count = 0
-    for e, elem in enumerate(crystal):
-        nb = crystal.get_neighbors(elem, 4)
-        if elem.specie.Z == 3:
-            cn = nn_calc.get_nn_info(crystal, e)
-            # print(cn)
-            min_dist1 = 1000
-            min_dist2 = 1000
-            for neigh in cn:
-                ion = neigh['site'].specie.oxi_state
-                dist = crystal.get_distance(e, neigh['site_index'])
-                if ion < 0:
-                    if dist < min_dist1:
-                        min_dist1 = dist
-                elif neigh['site'].specie.Z == 3:
-                    if dist < min_dist2:
-                        min_dist2 = dist
-                # except AttributeError:
-            lasd += min_dist1
-            llsd += min_dist2
-            # print(cn)
-            li_count += 1
-            llb += sum([i.specie.Z  == 3 for i in nb])
-        else:
-            bond_count += len(nb)
-            sbi += sum([abs(elem.specie.X - i.specie.X) for i in nb])
-        if elem.specie.oxi_state <= 0:
-            anion_count += 1
-            afc += nn_calc.get_cn(crystal, e)
-
-    llb = llb / li_count
-    lasd = lasd / li_count
-    llsd = llsd / li_count
-    sbi = sbi / bond_count
-    afc = afc / anion_count
-
-def builder_data(crystal):
-    # print(crystal.formula)
-    composition = {i:0 for i in range(1, 119)}
-    denom = len(crystal)
-    
-    for elem in crystal:
-        e = elem.specie.Z
-        # print(type(e))
-        composition[e] += 1 / denom
-
-    # print(crystal.get_space_group_info()[0])
-    composition['space_group'] = crystal.get_space_group_info()[0]
-    return composition
-
-def iter_data(data_dir):
-    warnings.filterwarnings('ignore')
-    read_path = osp.join(data_base, data_dir)
-    for c, cif in enumerate(os.listdir(read_path)):
-        struc = osp.join(read_path, cif)
-        struc = Structure.from_file(struc)
-        struc.add_oxidation_state_by_guess()
-        # print(struc.species)
-        # builder_data(struc)
-        oracle_data(struc)
-        if c == 100:
-            break
-
 
 if __name__ == '__main__':
+    data_base = '/Users/divya-sh/Documents/' \
+                'MILA - AI for Materials/data/'
+
     destination = 'li-ssb'
+
 
     query_crit = {
         "elements":{
@@ -100,6 +99,8 @@ if __name__ == '__main__':
     }
 
     # extra_properties = []
-    # download(query_crit, extra_properties, destination)
+    # download(query_crit, extra_properties, osp.join(data_base, destination))
 
-    iter_data(destination)
+    dataObj = CrystalDataset(osp.join(data_base, destination))
+    for d in dataObj:
+        print(d[0])
