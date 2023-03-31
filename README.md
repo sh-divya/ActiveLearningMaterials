c5d5c98b593361e55b035c174eb6661b835870a2
divya.sharma <divya.sharma@login-2.server.mila.quebec>
2023-03-31 13:36:39 -0400

    readme; still working on dataset class for baselines

---
+++ README.md
@@ -0,0 +1,20 @@
+This repository includes models and datasets to train and use as proxies as part of a GFlowNet pipline. The aim is to construct/sample crystal objects with a probability that is porportioanl to the reward calculated from the output of the proxy model. Possible targets include 'formation energy per atom' or 'ionic conductivity',
+
+## Requirements
+
+The code runs on python=3.9, and the required packages can be installed using
+
+'''bash
+pip install -r requirementsi\_materials.txt
+'''
+
+## Ionic Conductivity
+
+Dataset can downloaded using functions in proxies/crystal\_data.py
+The model can then be trained using run.py
+
+## CDVAE Baseline Datasets
+
+The CDVAE [paper](https://arxiv.org/abs/2110.06197) and repository provides 3 datasets that we will use as baselines for GFlowNet training. The dataset code is under proxies/data.py and can be trained using proxy.py.
+
+The 'config' folder will contain configurations/hyperparameter dictionaries to search over or use and train.
--- proxies/data.py
+++ proxies/data.py
@@ -32,33 +32,56 @@ class CrystalFeat(Dataset):
         ]
         self.xtransform = scalex
         self.ytransform = scaley
-        if write:
-            for sub in self.csv.keys():
-                self.write_data_csv(sub)
-        else:
-            self.data_df = pd.read_csv(osp.join(self.write_path, subset + "_data.csv"))
-            self.y = self.data_df[target].values
-            sub_cols = [
-                col for col in self.data_df.columns if col not in self.cols_of_interest
-            ]
-            self.x = self.data_df[sub_cols].values
+        self.data_df = pd.read_csv(osp.join(self.write_path, subset + "_data.csv"))
+        self.y = self.data_df[target].values
+        sub_cols = [
+            col for col in self.data_df.columns if col not in self.cols_of_interest
+        ]
+        self.x = self.data_df[sub_cols].values
 
-    def write_data_csv(self, subset="train"):
-        proxy_features = {
-            "Space Group": [],
-            "a": [],
-            "b": [],
-            "c": [],
-            "alpha": [],
-            "beta": [],
-            "gamma": [],
-        }
+    def __len__(self):
+        return self.x.shape[0]
 
-        for i in range(1, 119):
-            proxy_features[Element("H").from_Z(i).symbol] = []
-        id_cif_prop = pd.read_csv(self.csv[subset])
+    def __getitem__(self, idx):
+        mat = torch.from_numpy(self.x[idx, 1:])
+        target = torch.tensor((self.y[idx]))
+        if self.xtransform:
+            mat = (mat - self.xtransform["mean"]) / self.xtransform["std"]
+        if self.ytransform:
+            target = (target - self.ytransform["mean"]) / self.ytransform["std"]
+        return torch.nan_to_num(mat, nan=0.0), target
+
+
+def write_data_csv(root):
+    data_path = osp.join(root, 'data')
+    subsets = ['train', 'val', 'test']
+    proxy_features = {
+        "Space Group": [],
+        "a": [],
+        "b": [],
+        "c": [],
+        "alpha": [],
+        "beta": [],
+        "gamma": [],
+    }
+
+    cols_of_interest = [
+        "material_id",
+        "heat_all",
+        "heat_ref",
+        "formation_energy_per_atom",
+        "band_gap",
+        "e_above_hull",
+        "energy_per_atom",
+    ]
+    for i in range(1, 119):
+        proxy_features[Element("H").from_Z(i).symbol] = []
+    master_df = []
+    sub_lens = []
+    for sub in subsets:
+        id_cif_prop = pd.read_csv(osp.join(data_path, sub + '.csv'))
         df_cols = id_cif_prop.columns
-        sub_cols = [col for col in self.cols_of_interest if col in df_cols]
+        sub_cols = [col for col in cols_of_interest if col in df_cols]
         sub_df = id_cif_prop[sub_cols]
         for idx, row in id_cif_prop.iterrows():
             struc = Structure.from_str(row["cif"], fmt="cif")
@@ -81,36 +104,34 @@ class CrystalFeat(Dataset):
                     proxy_features[k].append(0)
         lens = [len(val) for k, val in proxy_features.items()]
         df = pd.DataFrame.from_dict(proxy_features)
-        df = pd.concat([sub_df, df.loc[:, (df != 0).any()]], axis=1)
-        df = df.astype({"material_id": "str"})
-        df.to_csv(osp.join(self.write_path, subset + "_data.csv"))
-
-    def __len__(self):
-        return self.x.shape[0]
-
-    def __getitem__(self, idx):
-        mat = torch.from_numpy(self.x[idx, 1:])
-        target = torch.tensor((self.y[idx]))
-        if self.xtransform:
-            mat = (mat - self.xtransform["mean"]) / self.xtransform["std"]
-        if self.ytransform:
-            target = (target - self.ytransform["mean"]) / self.ytransform["std"]
-        return torch.nan_to_num(mat, nan=0.0), target
-
+        sub_df = sub_df.astype({"material_id": "str"})
+        master_df.append(sub_df)
+        sub_lens.append(len(sub_df))
+    master_df = pd.concat(master_df, axis=0, ignore_index=True)
+    master_df = pd.concat([master_df, df.loc[:, (df != 0).any()]], axis=1)
+    for i, l in enumerate(sub_lens):
+        if i == 0:
+            low = 0
+        else:
+            low = sub_lens[i - 1]
+        high = l + low
+        df = master_df.loc[low: high, :]
+        df.to_csv(osp.join(root, subsets[i] + "_data.csv"))
 
 if __name__ == "__main__":
     folder = "./mp20"
-    xt = {
-        "mean": torch.load(osp.join(folder, "x.mean")),
-        "std": torch.load(osp.join(folder, "x.std")),
-    }
-    yt = {
-        "mean": torch.load(osp.join(folder, "y.mean")),
-        "std": torch.load(osp.join(folder, "y.std")),
-    }
-    temp = CrystalFeat(
-        root=folder, target="formation_energy_per_atom", subset="train", write=False, scalex=xt, scaley=yt)
-    print(temp[10][0].shape)
+    write_data_csv(folder)
+    # xt = {
+    #     "mean": torch.load(osp.join(folder, "x.mean")),
+    #     "std": torch.load(osp.join(folder, "x.std")),
+    # }
+    # yt = {
+    #     "mean": torch.load(osp.join(folder, "y.mean")),
+    #     "std": torch.load(osp.join(folder, "y.std")),
+    # }
+    # temp = CrystalFeat(
+    #     root=folder, target="formation_energy_per_atom", subset="train", write=False, scalex=xt, scaley=yt)
+    # print(temp[10][0].shape)
     # loader = DataLoader(temp, batch_size=27136)
     # for x, y in loader:
     #     m = y.mean(dim=0)
