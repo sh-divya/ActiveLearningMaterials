43bbf97bf21f36601e70e57c2d28c132ced85e42
vict0rsch <vsch@pm.me>
2023-04-04 15:47:47 -0400

    refactor configs & grid search

--- run.py
+++ run.py
@@ -1,18 +1,27 @@
 import copy
 import random
 
+import numpy as np
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-import numpy as np
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
 from torch.nn.init import xavier_uniform_
 from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
+from config.mp20 import config
 from proxies.data import CrystalFeat
 from proxies.models import ProxyMLP, ProxyModel
-from config.mp20 import config
+from utils.callbacks import get_checkpoint_callback
+from utils.misc import (
+    flatten_grid_search,
+    get_run_dir,
+    merge_dicts,
+    print_config,
+    resolve,
+)
+from utils.parser import parse_args_to_dict
 
 SEED = 0
 torch.manual_seed(SEED)
@@ -78,19 +87,43 @@ def train(config, logger):
         logger.experiment.finish()
 
 
-def main(config):
-    model_config = copy.copy(config["model_config"])
-    tune_var = config["tune_var"]
-
-    for var in model_config[tune_var]:
-        config["model_config"][tune_var] = var
-        name = [
-            key + "-" + str(config["model_config"][key]) for key in ["lr", "batch_size"]
-        ]
-        # logger = None
-        logger = WandbLogger(project="Proxy-MP20", name="_".join(name))
-        train(config, logger=logger)
-
-
 if __name__ == "__main__":
-    main(config)
+    # parse command-line arguments as `--key=value` and merge with config
+    # allows for nested dictionaries: `--key.subkey=value``
+    cli_conf = parse_args_to_dict()
+    config["run_dir"] = get_run_dir()
+    config = merge_dicts(config, cli_conf)
+    config["run_dir"] = resolve(config["run_dir"])
+    config["run_dir"].mkdir(parents=True, exist_ok=True)
+    config["run_dir"] = str(config["run_dir"])
+
+    print_config(config)
+
+    model_config = copy.copy(config["model_config"])
+    if config.get("model_grid_search"):
+        grid = flatten_grid_search(config["model_grid_search"])
+    else:
+        grid = [{}]
+
+    for m, model_grid_conf in enumerate(grid):
+        if len(grid) > 1:
+            print("\n" + "=" * 80)
+            print("=" * 80)
+            print_config(model_grid_conf)
+
+        config["model_config"] = merge_dicts(model_config, model_grid_conf)
+        name = "_".join(
+            [f"{key}-{config['model_config'][key]}" for key in ["lr", "batch_size"]]
+        )
+        # or "_".join([f"{k}-{v}" for k, v in model_grid_conf.items()])
+
+        if not config.get("no_logger"):
+            logger = WandbLogger(
+                project="Proxy-MP20",
+                name=(name),
+                entity="mila-ocp",
+            )
+        else:
+            logger = None
+
+        train(config, logger=logger)
