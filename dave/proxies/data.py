5ef77f8f2b76d128b282a3ab48e8db8836c210c9
divya.sharma <divya.sharma@login-1.server.mila.quebec>
2023-08-29 16:29:06 -0400

    Minimial changes to work on matbench data

--- dave/proxies/data.py
+++ dave/proxies/data.py
@@ -1,8 +1,28 @@
 import os
+import sys
+import gzip
 import torch
+import requests
+import tempfile
 import pandas as pd
 import os.path as osp
+from pathlib import Path
+from typing import Callable, List, Any, Sequence
+from pymatgen.core.structure import Structure
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+
 from torch.utils.data import Dataset, DataLoader
+from torch_geometric.data import Data, download_url
+from torch_geometric.data import InMemoryDataset
+from torch_geometric.loader import DataLoader as GraphLoader
+
+
+from dave.utils.atoms_to_graph import (
+    AtomsToGraphs,
+    pymatgen_structure_to_graph,
+    collate,
+    compute_neighbors,
+)
 
 
 class CrystalFeat(Dataset):
@@ -19,13 +39,16 @@ class CrystalFeat(Dataset):
             "band_gap",
             "e_above_hull",
             "energy_per_atom",
+            "Eform",
+            "Band Gap",
+            "cif",
         ]
         self.xtransform = scalex
         self.ytransform = scaley
         data_df = pd.read_csv(osp.join(csv_path, subset + "_data.csv"))
         self.y = torch.tensor(data_df[target].values, dtype=torch.float32)
         sub_cols = [col for col in data_df.columns if col not in self.cols_of_interest]
-        x = torch.tensor(data_df[sub_cols].values, dtype=float)
+        x = torch.tensor(data_df[sub_cols].values)
         self.sg = x[:, 1].to(torch.int32)
         self.lattice = x[:, 2:8].float()
         self.composition = x[:, 8:].to(torch.int32)
@@ -52,30 +75,160 @@ class CrystalFeat(Dataset):
         return (comp, sg, lat), target
 
 
-if __name__ == "__main__":
-    folder = "./carbon"
-    # write_data_csv(folder)
-    # xt = {
-    #     "mean": torch.load(osp.join(folder, "x.mean")),
-    #     "std": torch.load(osp.join(folder, "x.std")),
-    # }
-    # yt = {
-    #     "mean": torch.load(osp.join(folder, "y.mean")),
-    #     "std": torch.load(osp.join(folder, "y.std")),
-    # }
-    temp = CrystalFeat(
-        root=folder, target="energy_per_atom", subset="train"
-    )  # , scalex=xt, scaley=yt)
-    bs = len(temp)
-    print(temp[10][0])
-    loader = DataLoader(temp, batch_size=100)
-    for x, y in loader:
-        # m1 = x[-1].mean(dim=0)
-        # s1 = x[-1].std(dim=0)
-        torch.save(m1, osp.join(folder, "x.mean"))
-        torch.save(s1, osp.join(folder, "x.std"))
+class CrystalGraph(InMemoryDataset):
+    def __init__(
+        self,
+        root,
+        transform=None,
+        pre_transform=None,
+        pre_filter=None,
+        name="mp20",
+        subset="train",
+    ):
+        self.name = name
+        self.subset = subset
+        self.a2g = AtomsToGraphs(
+            max_neigh=50,
+            radius=6.0,
+            r_energy=False,
+            r_forces=False,
+            r_distances=True,
+            r_edges=False,
+        )
+        if transform is not None:
+            self.xtransform = transform["x"]
+            self.ytransform = transform["y"]
+            transform = None
+        else:
+            self.xtransform = None
+            self.ytransform = None
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
 
-        # m2 = y.mean(dim=0)
-        # s2 = y.std(dim=0)
-        # torch.save(m2, osp.join(folder, "y.mean"))
-        # torch.save(s2, osp.join(folder, "y.std"))
+    @property
+    def raw_dir(self) -> str:
+        check_path = Path(self.root) / "data" / f"{self.name}"
+        return str(check_path)
+
+    @property
+    def processed_dir(self) -> str:
+        check_path = Path(self.root) / "dave" / "proxies" / f"{self.name}"
+        return str(check_path)
+
+    @property
+    def raw_file_names(self):
+        return [f"{self.subset}_data.csv"]
+
+    @property
+    def processed_file_names(self):
+        return [self.subset + ".pt"]
+
+    def download(self):
+        raw_parent = Path(self.raw_dir).parent
+        temp_raw = Path(self.raw_dir) / "data"
+        temp_raw.mkdir(parents=True, exist_ok=True)
+        if self.name == "mp20":
+            from cdvae_csv import write_data_csv
+
+            download_url(
+                f"https://raw.githubusercontent.com/txie-93/cdvae/main/data/mp_20/train.csv",
+                temp_raw,
+            )
+            download_url(
+                f"https://raw.githubusercontent.com/txie-93/cdvae/main/data/mp_20/val.csv",
+                temp_raw,
+            )
+            download_url(
+                f"https://raw.githubusercontent.com/txie-93/cdvae/main/data/mp_20/test.csv",
+                temp_raw,
+            )
+            write_data_csv(self.raw_dir)
+        if self.name == "matbench_mp_e_form":
+            from dave.utils.mb_data_process import write_dataset_csv, split
+
+            json_file = raw_parent / "matbench_mp_e_form.json"
+            if not json_file.is_file():
+                with open(str(json_file), "wb") as j:
+                    r = requests.get(
+                        "https://ml.materialsproject.org/projects/matbench_mp_e_form.json.gz"
+                    )
+                    j.write(gzip.decompress(r.content))
+            if not (Path(self.raw_dir) / f"data/{self.name}.csv").is_file():
+                try:
+                    write_dataset_csv(
+                        [
+                            "--read_path",
+                            str(raw_parent),
+                            "--write_base",
+                            str(raw_parent),
+                            "--data",
+                            "0",
+                        ]
+                    )
+                except SystemExit as err:
+                    split(["--base_path", raw_parent, "--data_select", "0"])
+                    return
+            else:
+                split(["--base_path", raw_parent, "--data_select", "0"])
+
+    def process(self):
+        data_df = pd.read_csv(osp.join(self.raw_dir, self.raw_file_names[0]))
+        data_list = []
+        proc_dir = Path(self.processed_dir)
+        proc_dir.mkdir(parents=True, exist_ok=True)
+        for idx, row in data_df.iterrows():
+            struct = Structure.from_str(row["cif"], fmt="cif")
+            if self.name == "mp20":
+                target = "formation_energy_per_atom"
+                SGA = SpacegroupAnalyzer(struct)
+                struct = SGA.get_conventional_standard_structure()
+            else:
+                target = "Eform"
+            y = torch.tensor(row[target], dtype=torch.float32)
+            not_comp_cols = [
+                "cif",
+                target,
+                "Space Group",
+                "a",
+                "b",
+                "c",
+                "alpha",
+                "beta",
+                "gamma",
+                "material_id",
+                "band_gap",
+                "e_above_hull",
+            ]
+            data = pymatgen_structure_to_graph(struct, self.a2g)
+            if self.ytransform is not None:
+                data.y = ((y - self.ytransform["mean"]) / self.ytransform["std"]).to(
+                    torch.float32
+                )
+            else:
+                data.y = y
+            comp = []
+            for col in data_df.columns[1:]:
+                if col not in not_comp_cols:
+                    comp.append(row[col])
+            data.comp = torch.tensor(comp, dtype=torch.int32)
+            lp = torch.tensor(
+                [row[i] for i in ["a", "b", "c", "alpha", "beta", "gamma"]],
+                dtype=torch.int32,
+            )
+            if self.xtransform is not None:
+                data.lp = (lp - self.xtransform["mean"] / self.xtransform["std"]).to(
+                    torch.float32
+                )
+            else:
+                data.lp = lp
+            data.sg = torch.tensor(row["Space Group"], dtype=torch.int32)
+            data_list.append(data)
+        data, slices = self.collate(data_list)
+        # data, slices = collate(data_list)
+        Path(self.processed_paths[0]).parent.mkdir(exist_ok=True, parents=True)
+        torch.save((data, slices), self.processed_paths[0])
+
+    def get(self, idx):
+        data = super().get(idx)
+        data.neighbors = compute_neighbors(data, data.edge_index)
+        return data
--- dave/utils/loaders.py
+++ dave/utils/loaders.py
@@ -1,41 +1,58 @@
 from torch.utils.data import DataLoader
-
-from dave.proxies.data import CrystalFeat
+from torch_geometric.loader import DataLoader as GraphLoader
+from dave.proxies.data import CrystalFeat, CrystalGraph
 from dave.utils.misc import ROOT
-
+from pathlib import Path
+from copy import copy
 
 def make_loaders(config):
+    model, data = config["config"].split("-")
+    if not config.get("root"):
+        root = copy(ROOT)
+    else:
+        root = Path(config["root"]).resolve()
+
     if config["config"].endswith("-mp20"):
+        name = "mp20"
+        pass
+    elif config["config"].endswith("-mbform"):
+        name = "matbench_mp_e_form"
+        config["model"]["input_len"] = 91
+    elif config["config"].endswith("-mbgap"):
+        config["model"]["input_len"] = 91
+    else:
+        raise ValueError(f"Unknown config: {config['config']}")
+
+    if model == "fae":
+        load_class = GraphLoader
+        trainset = CrystalGraph(
+            str(config["root"]), name=name, subset="train", transform=config["scales"]
+        )
+        valset = CrystalGraph(
+            str(config["root"]), name=name, subset="val", transform=config["scales"]
+        )
+    else:
+        load_class = DataLoader
         trainset = CrystalFeat(
-            root=config["src"].replace("$root", str(ROOT)),
-            target="formation_energy_per_atom",
+            root=config["src"].replace("$root", str(root)),
+            target=config["target"],
             subset="train",
             scalex=config["scales"]["x"],
             scaley=config["scales"]["y"],
         )
         valset = CrystalFeat(
-            root=config["src"].replace("$root", str(ROOT)),
+            root=config["src"].replace("$root", str(root)),
             target=config["target"],
             subset="val",
             scalex=config["scales"]["x"],
             scaley=config["scales"]["y"],
         )
 
-        return {
-            "train": DataLoader(
-                trainset,
-                batch_size=config["optim"]["batch_size"],
-                shuffle=True,
-                pin_memory=True,
-                num_workers=config["optim"].get("num_workers", 0),
-            ),
-            "val": DataLoader(
-                valset,
-                batch_size=config["optim"]["batch_size"],
-                shuffle=False,
-                pin_memory=True,
-                num_workers=config["optim"].get("num_workers", 0),
-            ),
-        }
-
-    raise ValueError(f"Unknown config: {config['config']}")
+    return {
+        "train": load_class(
+            trainset, batch_size=config["optim"]["batch_size"], shuffle=True
+        ),
+        "val": load_class(
+            valset, batch_size=config["optim"]["batch_size"], shuffle=False
+        ),
+    }
