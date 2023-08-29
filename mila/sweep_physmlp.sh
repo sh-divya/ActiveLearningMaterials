db13e0028f0696bef2cf76fc5e6f6fb0fe79c195
Victor Schmidt <vsch@pm.me>
2023-08-29 17:35:40 -0400

    update phys-mlp-sweep

--- mila/sweep_physmlp.sh
+++ mila/sweep_physmlp.sh
@@ -2,10 +2,10 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --mem=32GB
-#SBATCH --output='/network/scratch/a/alexandre.duval/ocp/runs/sweep/output-%j.out'
-#SBATCH --error='/network/scratch/a/alexandre.duval/ocp/runs/sweep/error-%j.out'
+#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/ua95pu8h/output-%j.out
+#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/ua95pu8h/error-%j.out
 
-module load anaconda/3
-conda activate crystals
-cd ~/ocp/ActiveLearningMaterials
-wandb agent --count 75 mila-ocp/ActiveLearningMaterials/0cyp4agl  # sweep_id
+module load anaconda/3 cuda/11.7
+conda activate crystal-proxy
+cd /home/mila/s/schmidtv/ocp-project/ActiveLearningMaterials
+wandb agent --count 150 mila-ocp/ActiveLearningMaterials/ua95pu8h  # sweep_id
