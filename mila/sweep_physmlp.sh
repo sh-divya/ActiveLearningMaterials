5fe10f28365f3dce5bad4114496ccd50dba6aee9
Victor Schmidt <vsch@pm.me>
2023-08-29 21:25:01 -0400

    update sweep config

--- mila/sweep_physmlp.sh
+++ mila/sweep_physmlp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --mem=32GB
-#SBATCH --partition=main
-#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/s69ywif6/output-%j.out
-#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/s69ywif6/error-%j.out
+#SBATCH --mem=24GB
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=4
+#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/b9xq7rhh/output-%j.out
+#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/b9xq7rhh/error-%j.out
 
 module load anaconda/3 cuda/11.7
 conda activate crystal-proxy
 cd /home/mila/s/schmidtv/ocp-project/run-repos/ActiveLearningMaterials-dev
-wandb agent --count 150 mila-ocp/Dave-MBform/s69ywif6
+wandb agent --count 150 mila-ocp/Dave-MBform/b9xq7rhh
