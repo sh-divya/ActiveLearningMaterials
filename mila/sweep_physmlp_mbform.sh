a3fb39405d6e0a1481f55ed4237d2e21eca798f5
Victor Schmidt <vsch@pm.me>
2023-09-25 00:47:55 -0400

    update sweep id

--- mila/sweep_physmlp_mbform.sh
+++ mila/sweep_physmlp_mbform.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=16GB
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=2
-#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/1ibghlxg/output-%j.out
-#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/1ibghlxg/error-%j.out
+#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/8hc8nvb7/output-%j.out
+#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/8hc8nvb7/error-%j.out
 
 module load anaconda/3 cuda/11.7
 conda activate crystal-proxy
@@ -16,4 +16,4 @@ echo "• Current working directory: $(pwd)"
 echo "• Current branch: $(git rev-parse --abbrev-ref HEAD)"
 echo "• Current commit: $(git rev-parse HEAD)"
 
-wandb agent --count 50 mila-ocp/Dave-MBform/1ibghlxg
+wandb agent --count 50 mila-ocp/Dave-MBform/8hc8nvb7
