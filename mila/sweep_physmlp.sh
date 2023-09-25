65f0be7cf2bec14926dcd79faf45bda692ce3668
Victor Schmidt <vsch@pm.me>
2023-09-25 00:19:13 -0400

    update sbatch template

--- mila/sweep_physmlp.sh
+++ mila/sweep_physmlp.sh
@@ -4,10 +4,16 @@
 #SBATCH --mem=16GB
 #SBATCH --partition=long
 #SBATCH --cpus-per-task=2
-#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/osfcmbqt/output-%j.out
-#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/osfcmbqt/error-%j.out
+#SBATCH --output=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/1ibghlxg/output-%j.out
+#SBATCH --error=/network/scratch/s/schmidtv/crystals-proxys/runs/sweeps/1ibghlxg/error-%j.out
 
 module load anaconda/3 cuda/11.7
 conda activate crystal-proxy
 cd /home/mila/s/schmidtv/ocp-project/run-repos/ActiveLearningMaterials-dev
-wandb agent --count 50 mila-ocp/Dave-MBform/osfcmbqt
+
+echo "Current config:"
+echo "• Current working directory: $(pwd)"
+echo "• Current branch: $(git rev-parse --abbrev-ref HEAD)"
+echo "• Current commit: $(git rev-parse HEAD)"
+
+wandb agent --count 50 mila-ocp/Dave-MBform/1ibghlxg
