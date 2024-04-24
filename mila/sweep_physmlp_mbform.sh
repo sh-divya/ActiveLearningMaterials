178fcf036e0199d2f42674437f86505c74fcbc3d
Victor Schmidt <vsch@pm.me>
2024-04-24 15:21:36 -0400

    updated sweep script for victor

--- mila/sweep_physmlp_mbform.sh
+++ mila/sweep_physmlp_mbform.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=sweep-physmlp_mbform
+#SBATCH --partition=main
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=32GB
-#SBATCH --output=/network/scratch/d/divya.sharma/dave/sweep/output-%j.out
-#SBATCH --error=/network/scratch/d/divya.sharma/dave/sweep/error-%j.out
+#SBATCH --mem=24GB
+#SBATCH --output=/network/scratch/s/schmidtv/dave/logs/sweeps/output-%j.out
+#SBATCH --error=/network/scratch/s/schmidtv/dave/logs/sweeps/error-%j.out
 
-module load python/3.9
-source $HOME/venvs/materials/bin/activate
-cp -r $SCRATCH/ActiveLearningMaterials $SLURM_TMPDIR
+module load anaconda/3 cuda/11.7
+conda activate crystal-proxy
+cp -r /home/mila/s/schmidtv/ocp-project/ActiveLearningMaterials $SLURM_TMPDIR
 cd $SLURM_TMPDIR/ActiveLearningMaterials
-wandb agent --count 75 mila-ocp/Dave-MBform/s5p93f7h  # sweep_id
+wandb agent --count 75 mila-ocp/Dave-MBform/1vvgrwkt  # sweep_id
