80192862c8a8b5d20e65ff8357423612a8ba5a15
Victor Schmidt <vsch@pm.me>
2024-04-24 15:01:20 -0400

    add `SpaceGroupEncoder` and use in `ProxyEmbeddingModel`

--- dave/proxies/models.py
+++ dave/proxies/models.py
@@ -1,15 +1,24 @@
-import torch.nn as nn
+from pathlib import Path
+
 import torch
+import torch.nn as nn
 from phast.embedding import PhysEmbedding
-from torch_scatter import scatter
 from torch_geometric.nn.dense import DenseGATConv, DenseGCNConv
-from torch_geometric.nn.norm import GraphNorm
 from torch_geometric.utils import to_dense_batch
+from torch_scatter import scatter
+from yaml import safe_load
+
+from dave.utils.symmetries import all_symmetry_vectors
 
 
 def weights_init(m):
     if isinstance(m, nn.Linear):
         nn.init.xavier_uniform_(m.weight)
+
+
+def nan_grad_to_zero(grad):
+    grad[grad != grad] = 0
+    return grad
 
 
 def make_model(config):
@@ -36,7 +45,9 @@ def make_model(config):
             sg_emb_size=config["model"]["sg_emb_size"],
             comp_phys_embeds=config["model"]["comp_phys_embeds"],
             alphabet=config["alphabet"],
+            sg_encoder_config=config["model"].get("sg_encoder", {}),
         )
+        model.apply(weights_init)
         return model
     # Graph model without 3D pos.
     elif config["config"].startswith("graph-"):
@@ -176,9 +187,12 @@ class ProxyEmbeddingModel(nn.Module):
         lat_hidden_channels: int,
         sg_emb_size: int,
         alphabet: list = [],
+        sg_encoder_config: dict = {},
     ):
         super().__init__()
         self.use_comp_phys_embeds = comp_phys_embeds["use"]
+
+        # Physical embeddings for atom types
         if self.use_comp_phys_embeds:
             self.phys_emb = PhysEmbedding(
                 z_emb_size=comp_phys_embeds["z_emb_size"],
@@ -192,14 +206,29 @@ class ProxyEmbeddingModel(nn.Module):
             self.comp_emb_mlp = mlp_from_layers(
                 comp_num_layers, comp_hidden_channels, comp_size
             )
-        self.sg_emb = nn.Embedding(231, sg_emb_size)
+
+        # Symmetry-based embeddings for space groups
+        if sg_encoder_config.get("use"):
+            self.sg_emb = SpaceGroupEncoder(**sg_encoder_config)
+            sg_emb_size = self.sg_emb.output_size
+        else:
+            # basic space groups embedding
+            self.sg_emb = nn.Embedding(231, sg_emb_size)
+
+        # lattice parameters MLP
         self.lat_emb_mlp = mlp_from_layers(
             lat_num_layers, lat_hidden_channels, lat_size
         )
+
+        # compute full embedding size
         self.pred_inp_size = comp_hidden_channels + sg_emb_size + lat_hidden_channels
+
+        # output MLP
         self.prediction_head = ProxyMLP(
             self.pred_inp_size, pred_num_layers, pred_hidden_channels, False
         )
+
+        # Alphabet mapping {index: atomic_number}
         if not alphabet:
             self._alphabet = torch.Tensor(list(range(comp_size)))
         else:
@@ -224,22 +253,23 @@ class ProxyEmbeddingModel(nn.Module):
                 torch.arange(comp_x.shape[0]).to(comp_x.device),
                 comp_x.sum(dim=1).long(),
             )
-            comp_x = self.phys_emb(self.alphabet[z].to(torch.int32))
-            comp_x = scatter(comp_x, batch_mask, dim=0, reduce="mean")
+            # comp_x = self.phys_emb(self.alphabet[z].to(torch.int32))
+            comp_x = self.phys_emb(z).to(torch.int32)
+            comp_h = scatter(comp_x, batch_mask, dim=0, reduce="mean")
         else:
-            comp_x = self.comp_emb_mlp(comp_x)
+            comp_h = self.comp_emb_mlp(comp_x)
 
         # Process the space group
-        sg_x = self.sg_emb(sg_x).squeeze(1)
+        sg_h = self.sg_emb(sg_x).squeeze(1)
 
         # Process the lattice
-        lat_x = self.lat_emb_mlp(lat_x)
+        lat_h = self.lat_emb_mlp(lat_x)
 
         # TODO: add a counter for the number of atoms in the unit cell
 
         # Concatenate and predict
-        x = torch.cat((comp_x, sg_x, lat_x), dim=-1)
-        return self.prediction_head(x)
+        h = torch.cat((comp_h, sg_h, lat_h), dim=-1)
+        return self.prediction_head(h)
 
 
 class ProxyGraphModel(nn.Module):
@@ -352,3 +382,241 @@ class ProxyGraphModel(nn.Module):
         # Concatenate and predict
         x = torch.cat((comp_x, sg_x, lat_x), dim=-1)
         return self.prediction_head(x)
+
+
+base = "/network/projects/crystalgfn/data/temporary_files/gflownet-dev-yamls"
+FILES = {
+    "ps": str(Path(base) / "point_symmetries.yaml"),
+    "cls": str(Path(base) / "crystal_lattice_systems.yaml"),
+}
+
+
+class SpaceGroupEncoder(nn.Module):
+    def __init__(
+        self,
+        symmetries_hiddens=[16],
+        symmetries_aggregation="sum",
+        space_group_size=16,
+        point_symmetry_size=16,
+        cl_system_size=16,
+        **kwargs,
+    ):
+        """
+        Space group encoder.
+
+        Args:
+            symmetries_hiddens (list, optional): Hidden layers for the symmetry
+                vectors. Defaults to [16].
+            symmetries_aggregation (str, optional): Aggregation stratey for the
+                symmetry vectors. Defaults to "sum".
+            space_group_size (int, optional): Space group embedding size.
+                Defaults to 16.
+            point_symmetry_size (int, optional): Point symmetry embedding size.
+                Defaults to 16.
+            cl_system_size (int, optional): Crystal lattice system embedding size.
+                Defaults to 16.
+            kwargs (dict): Additional ignored arguments.
+        """
+        super().__init__()
+
+        if isinstance(symmetries_hiddens, int):
+            symmetries_hiddens = [symmetries_hiddens]
+        elif isinstance(symmetries_hiddens, str):
+            symmetries_hiddens = [int(h.strip()) for h in symmetries_hiddens.split(",")]
+
+        self.symmetries_hiddens = symmetries_hiddens if symmetries_hiddens else []
+        self.space_group_size = int(space_group_size) if space_group_size else 0
+        self.point_symmetry_size = (
+            int(point_symmetry_size) if point_symmetry_size else 0
+        )
+        self.cl_system_size = int(cl_system_size) if cl_system_size else 0
+        self.symmetries_aggregation = symmetries_aggregation
+
+        self.symmetries_encoder = None
+        if symmetries_hiddens:
+            self.symmetries_encoder = []
+            sizes = [7] + symmetries_hiddens
+            for i, output_dim in enumerate(symmetries_hiddens):
+                input_dim = sizes[i]
+                self.symmetries_encoder.append(nn.Linear(input_dim, output_dim))
+                if i < len(symmetries_hiddens) - 1:
+                    self.symmetries_encoder.append(nn.SiLU())
+            self.symmetries_encoder = nn.Sequential(*self.symmetries_encoder)
+            for param in self.symmetries_encoder.parameters():
+                # prevent nan grads to 0 with register hook
+                param.register_hook(nan_grad_to_zero)
+
+        self.space_group_encoder = (
+            nn.Embedding(230 + 1, space_group_size) if space_group_size > 0 else None
+        )
+        self.point_symmetry_encoder = (
+            nn.Embedding(6 + 1, point_symmetry_size)
+            if point_symmetry_size > 0
+            else None
+        )
+        self.cl_system_encoder = (
+            nn.Embedding(8 + 1, point_symmetry_size)
+            if point_symmetry_size > 0
+            else None
+        )
+
+        # Make tensor that maps space groups to point symmetries
+        sg_to_ps_dict = safe_load(open(FILES["ps"]))
+        sg_to_ps_dict = {
+            sg: ps for ps, psd in sg_to_ps_dict.items() for sg in psd["space_groups"]
+        }
+        sg_to_ps = torch.full((max(sg_to_ps_dict.keys()) + 1,), -1, dtype=torch.long)
+        for sg, ps in sg_to_ps_dict.items():
+            sg_to_ps[sg] = ps
+        self.register_buffer("sg_to_ps", sg_to_ps)
+
+        # Make tensor that maps space groups to crystal lattice systems
+        sg_to_cls_dict = safe_load(open(FILES["cls"]))
+        sg_to_cls_dict = {
+            sg: cls
+            for cls, clsd in sg_to_cls_dict.items()
+            for sg in clsd["space_groups"]
+        }
+        sg_to_cls = torch.full((max(sg_to_cls_dict.keys()) + 1,), -1, dtype=torch.long)
+        for sg, cls in sg_to_cls_dict.items():
+            sg_to_cls[sg] = cls
+        self.register_buffer("sg_to_cls", sg_to_cls)
+
+        # Precompute symmetry vectors for all space groups
+        # Make 3D tensor with all symmetry vectors for all space groups
+        # Padding empty rows groups with NaNs
+        print("Precomputing space group symmetries...", end="", flush=True)
+        sg_to_sym_vects_dict = {
+            space_group: torch.from_numpy(all_symmetry_vectors(space_group)).float()
+            for space_group in range(1, 231)
+        }
+        max_n_vects = max([v.shape[0] for v in sg_to_sym_vects_dict.values()])
+        sg_to_sym_vects = torch.full(
+            (max(sg_to_sym_vects_dict.keys()) + 1, max_n_vects, 7), float("nan")
+        )
+        for sg, vects in sg_to_sym_vects_dict.items():
+            sg_to_sym_vects[sg, : vects.shape[0]] = vects
+        self.register_buffer("sg_to_sym_vects", sg_to_sym_vects)
+        print(" ok.")
+
+        self._output_size = 0
+
+    @property
+    def output_size(self):
+        if not self._output_size:
+            size = 0
+            if self.symmetries_encoder:
+                size += self.symmetries_hiddens[-1]
+            size += max(self.space_group_size, 0)
+            size += max(self.point_symmetry_size, 0)
+            size += max(self.cl_system_size, 0)
+            self._output_size = size
+        return self._output_size
+
+    def aggregate_symmetry_embeddings(self, embedded_symetry_vectors: torch.Tensor):
+        """
+        Aggregate symmetry embeddings according to ``self.symmetries_aggregation``.
+
+        Args:
+            embedded_symetry_vectors (torch.Tensor): Symmetry embeddings as [N, D]
+
+        Raises:
+            ValueError: If the aggregation method is unknown.
+
+        Returns:
+            torch.Tensor: Aggregated symmetry embeddings as [D]
+        """
+        if self.symmetries_aggregation == "sum":
+            return torch.nansum(embedded_symetry_vectors, 1)
+        elif self.symmetries_aggregation == "mean":
+            return torch.nanmean(embedded_symetry_vectors, 1)
+        raise ValueError(f"Unknown aggregation method: {self.symmetries_aggregation}")
+
+    def embed_symmetries(self, space_group: int):
+        """
+        Embed symmetry vectors for a given space group if ``self.symmetries_encoder`` is
+        available, then aggregate the embeddings according to
+        ``self.symmetries_aggregation``.
+
+        Args:
+            space_group (int): Space group number.
+
+        Returns:
+            torch.Tensor: Symmetry embeddings as [D] or ``None`` if the encoder is
+                not available.
+        """
+        if self.symmetries_encoder:
+            embeddings = self.symmetries_encoder(self.sg_to_sym_vects[space_group])
+            return self.aggregate_symmetry_embeddings(embeddings)
+
+    def embed_space_group(self, space_group: int):
+        """
+        Embed the space group number if ``self.space_group_encoder`` is available.
+
+        Args:
+            space_group (int): Space group number.
+
+        Returns:
+            torch.Tensor: Space group embedding as [D] or ``None`` if the encoder is
+                not available.
+        """
+        if self.space_group_encoder:
+            return self.space_group_encoder(space_group.long())
+
+    def embed_point_symmetry(self, space_group: int):
+        """
+        Embed the point symmetry if ``self.point_symmetry_encoder`` is available.
+
+        Args:
+            space_group (int): Space group number.
+
+        Returns:
+            torch.Tensor: Point symmetry embedding as [D] or ``None`` if the encoder is
+                not available.
+        """
+        if self.point_symmetry_encoder:
+            return self.point_symmetry_encoder(self.sg_to_ps[space_group])
+
+    def embed_cl_system(self, space_group: int):
+        """
+        Embed the crystal lattice system if ``self.cl_system_encoder`` is available.
+
+        Args:
+            space_group (int): Space group number.
+
+        Returns:
+            torch.Tensor: Crystal lattice system embedding as [D] or ``None`` if the
+                encoder is not available.
+        """
+        if self.cl_system_encoder:
+            return self.cl_system_encoder(self.sg_to_ps[space_group])
+
+    def forward(self, space_group: int, as_dict=False):
+        """
+        Embed all symmetries for a given space group and return the embeddings as a
+        concatenated tensor.
+
+        Order:
+        - Symmetries (as aggregated tensor)
+        - Point symmetry
+        - Crystal lattice system
+        - Space group
+
+        Args:
+            space_group (int): Space group number.
+            as_dict (bool, optional): Return the embeddings as a dict. Mainly to debug.
+                Defaults to False.
+
+        Returns:
+            torch.Tensor or list: Embeddings as a concatenated tensor or a list of
+                tensors.
+        """
+        embeddings = {
+            "syms": self.embed_symmetries(space_group),
+            "ps": self.embed_point_symmetry(space_group),
+            "cls": self.embed_cl_system(space_group),
+            "sg": self.embed_space_group(space_group),
+        }
+        if as_dict:
+            return embeddings
+        return torch.cat([h for h in embeddings.values() if h is not None], -1)
